d34335dfd3b6f13188bac4183ddaa9b2ba99335d
Pugpaprika21 <pugpaprika21@gmail.com>
2023-11-18 19:28:19 -0800

    create user controller api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -54,7 +55,7 @@ class UserController extends Controller
         }
         
         $user->username = $request->username;
-        $user->password = $request->password;
+        $user->password = Hash::make($request->password);
         $user->email = $request->email;
         $user->save();
 
